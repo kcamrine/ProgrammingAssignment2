69fc756feebb8dff434571a4b643502c9313d0d2
kcamrine <kc.amrine@gmail.com>
2014-11-19 16:27:06 -0800

    updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,31 +3,27 @@
 ## save it to an object, or retrieve the inverse if it has already been computed.
 
 ## To test this, one can run the following:
-## Make a matrix that can cache a value
+## Make a matrix that can cache a value (but does not have one yet)
 # x <- makeCacheMatrix(matrix(c(7,14,21,12,84,6),nrow=5,ncol=5))
-## Compute the inverse if it has not already done so
+## Compute and cache the inverse
 # cacheSolve(x)
-## this will return the inverse. Now, if it is already computed and you re-run
-## the previous command, you will get the output "getting cached data"
-## before it returns the inverse. 
+## this will return the inverse. Now, it is also saved. Re-run
+# cacheSolve(x)
+## you will get the output "getting cached data" before it returns 
+## the inverse (no re-computing). 
 
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-  # initialize m as nothing
-  m <- NULL
+  m <- NULL # initialize m as nothing
   # define internal functions
-  set <- function(y) {
+  set <- function(y) { # function to set the value of the matrix
     x <<- y
     m <<- NULL
   }
-  # will return the matrix
-  get <- function() x
-  # will cache the inverse to whatever is supplied
-  setinv <- function(inverse) m <<- inverse
-  # will return the inverse (initialized to NULL)
-  getinv <- function() m
-  # store the functions in a list
-  list(set = set, get = get,
+  get <- function() x   # function to return the matrix
+  setinv <- function(inverse) m <<- inverse   # will cache the inverse to whatever is supplied
+  getinv <- function() m   # will return the inverse, or NULL if it is not calculated 
+  list(set = set, get = get,   # store the functions in a list
        setinv = setinv,
        getinv = getinv)
 }
@@ -37,21 +33,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ##  makeCacheMatrix above. If the inverse has already been calculated (and 
 ##  the matrix has not changed), then the cacheSolve should retrieve the 
 ##  inverse from the cache.
+
 cacheSolve <- function(x, ...) {
-  # call internal function on makeCacheMatrix object. 
-  m <- x$getinv()
-  # If it exists, print message and value of m
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+  m <- x$getinv()   # call internal function  to get the inverse on makeCacheMatrix object. 
+  if(!is.null(m)) {   # If there is a value initialized for inverse    
+    message("getting cached data")  #print message 
+    return(m) # return value of the inverse and exit
   }
-  # if it doesnt exist (you don't hit the return function), continue and c
-  # get the matrix
-  data <- x$get()
+  data <- x$get() #get the matrix values from the object
   # compute the inverse
-  m <- solve(data, ...)
-  # then set the inverse 
-  x$setinv(m)
-  ## Return a matrix that is the inverse of 'x'
-  m
+  m <- solve(data, ...) #compute the inverse
+  x$setinv(m)   # then set the inverse to previously computed value 
+  m   ## Return a matrix that is the inverse of 'x'
 }
